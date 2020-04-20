95d8b51cdde46e36aa1f45524b10b3a9a0e6e6f5
Luca Santini <lucasantinidev@gmail.com>
2020-04-20 15:51:30 +0200

    domande all'utente

---
+++ main.js
@@ -0,0 +1,13 @@
+// quanti km l'utente deve percorrere
+var km = prompt("Quanti km vuoi percorrere", "100");
+console.log("100");
+// età del passeggero
+var age = prompt("Quanti anni hai?", "50");
+console.log("50");
+
+// calcolare prezzo del viaggio
+// var prezzo = 100 * 0.21;
+// console.log(100 * '0.21');
+ // prezzo del biglietto è definito in base ai km (0.21 € al km)
+ // va applicato uno sconto del 20% per i minorenni
+ // va applicato uno sconto del 40% per gli over 65
